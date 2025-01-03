e5a927e3489fcb30da0fc2220099d35f6d2da427
Hansib <balasooriyabmhp98@gmail.com>
2025-01-03 13:12:05 +0530

    Resolved merge conflicts: Add multiply and divide functions

--- calculator.js
+++ calculator.js
@@ -3,18 +3,18 @@ function add(a, b) {
     return a + b;
 }
 
-module.exports = { add };
-<<<<<<< HEAD
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
 function divide(a, b) {
     if (b === 0) throw new Error("Cannot divide by zero");
     return a / b;
 }
 
-module.exports = { add, subtract, divide };
-=======
-function subtract(a, b) {
-    return a - b;
-}
+module.exports = { add, subtract, multiply, divide };
 
-module.exports = { add, subtract };
->>>>>>> 5558a696d52d9d17a8ac5dd631c299dc8ea146d4
